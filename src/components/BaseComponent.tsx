ad593e61d812eabe5366a5da31856b376e4848f2
Jason <atwill007@163.com>
2020-06-21 16:34:22 +0800

    fix: scss样式在小程序中不生效的bug

--- src/components/BaseComponent.tsx
+++ src/components/BaseComponent.tsx
@@ -7,6 +7,7 @@ export default class BaseComponent<P = BaseProps, S = BaseState> extends Compone
   }
 
   static options = {
-    // addGlobalClass: true
+    // 全局样式类是微信小程序定义的一套用于修改组件内部样式的方案。如果希望组件外样式类能够影响组件内部，可以在组件构造器中的 options.addGlobalClass 字段设置为 true（Taro UI 的组件均开启了此特性）。这个特性从小程序基础库版本 2.2.3 开始支持
+    addGlobalClass: true
   }
 }
